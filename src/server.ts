5b68328ac5f146793634f591298fc734d29c3737
Davi Schilling <davi.bschilling@mobilesoft.it>
2020-09-18 16:28:30 +0200

    start project

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import 'module-alias/register'
+import express from 'express'
+import { json } from 'body-parser'
+
+const app = express()
+app.use(json())
+
+app.get('/api/hello', (req, res) => {
+  res.send('Hi there')
+})
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000')
+})
