ad0aa99c44553d9d53a3a50d41a516e2511f12d7
Davi Schilling <davi.bschilling@mobilesoft.it>
2020-09-23 11:01:30 +0200

    fixes

--- src/main/routes/hello-world.ts
+++ src/main/routes/hello-world.ts
@@ -1,7 +1,7 @@
-// import { adaptRoute } from '../../infra/express/express-route-adapter'
+import { adaptRoute } from '../../infra/express/express-route-adapter'
 import { Router } from 'express'
 // import { makeLoginFactory } from './../factories/auth/LoginFactory'
 
 export default (router: Router): void => {
-  // router.post('/login', adaptRoute(makeLoginFactory()))
+  router.post('/hello', adaptRoute(makeLoginFactory()))
 }
