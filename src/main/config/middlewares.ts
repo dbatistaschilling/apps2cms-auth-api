56b08ac711e635ca1772ef022dc4c083354b1777
davi schilling <davi.bschilling@mobilesoft.it>
2020-10-06 18:31:56 +0200

    refactor middleware imports

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,5 @@
 import { Express } from 'express'
-import { bodyParser, cors, contentType, cookie } from '../middlewares'
+import { bodyParser, cors, contentType, cookie } from '@wymaze/apps2cms-common'
 
 export default (app: Express): void => {
   app.set('trust proxy', true)
