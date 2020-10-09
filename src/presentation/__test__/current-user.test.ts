5780494df32536f1f537287d806709d082e5195a
davi schilling <davi.bschilling@mobilesoft.it>
2020-10-09 14:33:29 +0200

    add not authorized tests

--- src/presentation/__test__/current-user.test.ts
+++ src/presentation/__test__/current-user.test.ts
@@ -1,6 +1,13 @@
 import request from 'supertest'
 import app from '../../main/config/app'
 
+it('Responds with 401 not authorized if user is not logged in', async () => {
+  await request(app)
+    .get('/api/auth/current-user')
+    .send({})
+    .expect(401)
+})
+
 it('Responds with details about the current user', async () => {
   const cookie = await global.signup()
 
--- src/presentation/__test__/signout.test.ts
+++ src/presentation/__test__/signout.test.ts
@@ -1,6 +1,13 @@
 import request from 'supertest'
 import app from '../../main/config/app'
 
+it('Responds with 401 not authorized if user is not logged in', async () => {
+  await request(app)
+    .post('/api/auth/signout')
+    .send({})
+    .expect(401)
+})
+
 it('Clears the cookie after signout', async () => {
   const cookie = await global.signup()
 
